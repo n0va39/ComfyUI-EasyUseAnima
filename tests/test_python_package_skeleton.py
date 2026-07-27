681ec9bb16b54fba25c1575e8e0b4db3ddf42bb4
n0va39 <fulgensnova39@gmail.com>
2026-07-28 05:19:54 +0900

    refactor: move LoRA list route to API package (D-06c) (#489)

--- tests/test_python_package_skeleton.py
+++ tests/test_python_package_skeleton.py
@@ -49,6 +49,7 @@ PACKAGE_MODULES = (
     "easyuse_anima.api.routes",
     "easyuse_anima.api.routes.aio_torch_compile",
     "easyuse_anima.api.routes.autocomplete",
+    "easyuse_anima.api.routes.lora_catalog",
     "easyuse_anima.api.routes.long_text_settings",
     "easyuse_anima.api.routes.translation",
     "easyuse_anima.api.routes.translation_execution",
@@ -226,6 +227,9 @@ print(json.dumps({{
             "build_autocomplete_handlers",
             "build_classify_prompt_handler",
         ]
+        expected_all[
+            PACKAGE_MODULES.index("easyuse_anima.api.routes.lora_catalog")
+        ] = ["build_loras_handler"]
         expected_all[
             PACKAGE_MODULES.index("easyuse_anima.api.routes.long_text_settings")
         ] = ["build_long_text_settings_handlers"]
